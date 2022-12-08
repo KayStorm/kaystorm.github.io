a35bfcce0f572ab595a6e6766339cfb222e7e231
KayStorm <KayStormEmeralds@gmail.com>
2022-12-08 10:11:16 -0500

    Accurate calculations

--- documents/bdCalc.js
+++ documents/bdCalc.js
@@ -1,28 +1,33 @@
 const today = new Date();
-const birthday = new Date(1983, 11, 28, 3, 45, 00, 000);
-birthday.setFullYear(today.getFullYear());
+const birthday = new Date(2023, 08, 28, 3, 45, 00, 000);
+//birthday.setFullYear(today.getFullYear());
 const msPerDay = 24 * 60 * 60 * 1000;
 let daysLeft = (birthday.getTime() - today.getTime()) / msPerDay;
 daysLeft = Math.round(daysLeft);
 
 //check variables
 //document.write(today);
-document.write("My birthday is on " + birthday + ". ");
+document.write("My birthday is on September 28.");
 //document.write(daysLeft);
 
 //check for negative
-//negTest = Math.sign(daysLeft);
+negTest = Math.sign(daysLeft);
 //document.write(negTest);
 
-if ((negTest = 1)) {
+if (negTest == 1) {
 	document.write("You have " + daysLeft + " days!");
 } else {
+	/*
 	const endOfYear = new Date(1983, 12, 31, 24, 60, 60, 999);
 	endOfYear.setFullYear(today.getFullYear());
 	const msPerDay = 24 * 60 * 60 * 1000;
 	let daysLeftThisYear = (today.getTime() - endOfYear.getTime()) / msPerDay;
 	daysLeftThisYear = Math.round(daysLeftThisYear);
 	let updatedDaysLeft = daysLeft + daysLeftThisYear;
+	*/
 
-	document.write("You have " + updatedDaysLeft + " days!");
+	let updatedDaysLeft = birthday.getTime() - today.getFullYear();
+	updatedDaysLeft = Math.ceil(updatedDaysLeft / msPerDay);
+
+	document.write("You have " + updatedDaysLeft + " days!!");
 }
