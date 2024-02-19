cb1f341e522c1824f3998f2490d26204c44bdc9b
KayStorm <93082092+KayStorm@users.noreply.github.com>
2024-02-19 10:23:15 -0500

    Removing unnecessary comments

--- documents/bdCalc.js
+++ documents/bdCalc.js
@@ -1,14 +1,11 @@
 const today = new Date();
 const birthday = new Date(currentyear, 08, 28, 3, 45, 00, 000);
-//birthday.setFullYear(today.getFullYear());
+
 const msPerDay = 24 * 60 * 60 * 1000;
 let daysLeft = (birthday.getTime() - today.getTime()) / msPerDay;
 daysLeft = Math.round(daysLeft);
 
-//check variables
-//document.write(today);
 document.write("My birthday is on September 28.");
-//document.write(daysLeft);
 
 //check for negative
 negTest = Math.sign(daysLeft);
